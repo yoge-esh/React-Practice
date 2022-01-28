81968236cf03b7d51063d9f4df098978e7e0c1b5
yoge-esh <yogesh124khanal@gmail.com>
2022-01-28 15:55:49 +0545

    forms in React js

--- src/App.js
+++ src/App.js
@@ -2,33 +2,23 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  let color = '#8e44ae';
-  const [bg, setBg] = useState(color);
-  const [name, setName] = useState('Click me');
-  // const [doube, setDouble] = useState(name);
+    const [welcome, setWelcome] = useState('');
 
-  const bgChnage = () => {
-    console.log('clicke!')
-    setBg(color = 'yellow')
-    setName('Click me Back')
-  }
-  const bgback = () =>
-  {
-    console.log('double clicked');
-    setBg(color = '#8e44ae');
-    setName('we are back!')
-  }
-
-
-  return (
-    <div style={{ backgroundColor: bg }}>
-      <div>
-        <h2 className='content'>Welcome to Events in React Js </h2>
-        {/* <button  onClick ={bgChnage} onDoubleClick={bgback}>{name}</button> */}
-        <button  onMouseEnter ={bgChnage} onMouseLeave={bgback}>{name}</button>
-      </div>
-    </div>
-  );
+    const inputEvent = (event) =>{
+        console.log(event.target.value);
+    }
+    const submit = () =>{
+        setWelcome(inputEvent)
+    }
+    return (
+        <>
+            <div>
+                <h2>Welcome to React Form: {welcome} </h2 >
+                <input type='text' placeholder='Enter your Name...' onChange={inputEvent}/>
+                <button onClick={submit}>Click Me</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React  from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
+import App from './App'
+ReactDOM.render
+(
     <>
-        <App/>
-    </>,
+    <App/>
+    </>, 
     document.getElementById('root')
 )
