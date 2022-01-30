9cd410f3c803b8ad308c130bb5cf380a10aede93
yoge-esh <yogesh124khanal@gmail.com>
2022-01-30 12:36:30 +0545

    user input login form and display in same page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,80 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-    const [welcome, setWelcome] = useState('');
+    const [firstName, setFirstName] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        password: "",
+        textarea: "",
+    });
 
-    const inputEvent = (event) =>{
-        console.log(event.target.value);
-    }
-    const submit = () =>{
-        setWelcome(inputEvent)
-    }
+    const inputEvent = (event) => {
+        const { name, value } = event.target;
+        setFirstName((prevState) => {
+            return {
+                ...prevState,
+                [name]: value
+            };
+        });
+    };
+
+    const submit = (event) => {
+        event.preventDefault(); //prevent default action of form submit which is on page refresh
+        alert('form submmitted');
+    };
+
+
+
     return (
         <>
             <div>
-                <h2>Welcome to React Form: {welcome} </h2 >
-                <input type='text' placeholder='Enter your Name...' onChange={inputEvent}/>
-                <button onClick={submit}>Click Me</button>
+                <h2>Welcome to React Form: {firstName.fname} {firstName.lname}</h2>
+                <p>{firstName.email}</p>
+                <p>{firstName.password}</p>
+                <form onSubmit={submit}>
+                    <input
+                        type="text"
+                        placeholder="Enter your First Name..."
+                        onChange={inputEvent}
+                        value={firstName.fname}
+                        name="fname"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Enter your Last Name..."
+                        onChange={inputEvent}
+                        value={firstName.lname}
+                        name="lname"
+                    />
+                    <input
+                        type="email"
+                        placeholder="Enter your email..."
+                        onChange={inputEvent}
+                        value={firstName.email}
+                        name="email"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Enter your password..."
+                        onChange={inputEvent}
+                        value={firstName.password}
+                        name="password"
+                    />
+                    <textarea
+                        rows={4}
+                        cols={50}
+                        type="text"
+                        placeholder="Enter your message..."
+                        onChange={inputEvent}
+                        value={firstName.textarea}
+                        name="textarea"
+                    />
+
+
+                    <button type="submit">Click Me</button>
+                </form>
             </div>
         </>
     );
